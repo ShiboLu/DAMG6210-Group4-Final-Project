157f4e60f450fdb545a8c65e2b3b924b330f108c
Jashwanth Reddy Kamsani <90357714+kamsanij@users.noreply.github.com>
2022-04-18 10:15:41 -0400

    Add files via upload

---
+++ P4.-Database Implementation/Insert_Brand.sql
@@ -0,0 +1,18 @@
+
+INSERT INTO Brand_T VALUES
+	(0100001, 'Simply', 'Atlanta, GA', 'Coca Cola Company'),
+	(0100002, 'Tropicana', 'Bradenton, FL', 'Pepsico'),
+	(0100003, 'Coca cola', 'Atlanta, GA', 'Coca Cola Company'),
+	(0100004, 'Pepsi', 'Bradenton, FL', 'Pepsico'),
+	(0100005, 'Folgers', 'Orrville, OH', 'The J.M. Smucker Company'),
+	(0100006, 'Cafe Bustelo', 'Orrville, OH', 'The J.M. Smucker Company'),
+	(0100007, '2K Games', 'New York, NY', 'Take Two Interactive'),
+	(0100008, 'Electronic Arts', 'Redwood City, CA', 'Electronic Arts'),
+	(0100009, 'Welry', 'New York, NY', 'Richline Group'),
+	(0100010, 'Pandora', 'Baltimore, MD', 'Pandora Group'),
+	(0100011, 'Apple', 'Cupertino, CA', 'Apple Inc'),
+	(0100012, 'Eddie Bauer', 'Seattle, WA', 'Spark Group LLC'),
+	(0100013, 'SteelCase', 'Grand Rapis, MI', 'SteelCase'),
+	(0100014, 'Harry Potter', 'New York, NY', 'Scholastic Inc'),
+	(0100015, 'Garden of Life', 'Palm Beach, FL', 'Prostic Inc');
+
