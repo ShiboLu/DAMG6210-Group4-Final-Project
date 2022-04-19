032ca34d2a3b3ce9551b0fa06b7932d777be265f
Simrannagpurkar <99046138+Simrannagpurkar@users.noreply.github.com>
2022-04-19 13:47:58 -0400

    Delete SQLVIEWshipment_supplier.sql

--- P4.-Database Implementation/SQLVIEWshipment_supplier.sql
+++
@@ -1,6 +0,0 @@
-CREATE VIEW [SupplierShipmentDetails] AS
-SELECT SU.SupplierID, SU.SupplierName,SH.ShipmentID, SH.OrderDate, SH.EstimatedDeliveryDate, SH.ActualDeliveryDate
-FROM Supplier_T SU JOIN Shipment_T SH ON 
-SU.SupplierID = SH.SupplierID;
-
-SELECT * FROM SupplierShipmentDetails
