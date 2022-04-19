79fa4891abcd95be8cff9661c624fb0ce324623a
stanluu <42020235+stanluu@users.noreply.github.com>
2022-04-18 20:01:43 -0400

    Column Data Encryption
    CUSTOMER & EMPLOYEE

---
+++ P4.-Database Implementation/P4 Column Data Encryption-Employee password.sql
@@ -0,0 +1,47 @@
+use DAMG6210_G4
+GO
+
+alter table Employee_T add [password] varbinary(300)
+
+select *
+FROM sys.symmetric_keys;
+
+create MASTER KEY
+	ENCRYPTION BY PASSWORD = 'damg6210group04';
+
+SELECT name KeyName,
+  symmetric_key_id KeyID,
+  key_length KeyLength,
+  algorithm_desc KeyAlgorithm
+FROM sys.symmetric_keys;
+go
+
+CREATE CERTIFICATE EmpPass  
+   WITH SUBJECT = 'Employee Password';  
+GO  
+
+-- drop CERTIFICATE CusInfo  
+
+CREATE SYMMETRIC KEY EmpPass_SM
+	WITH ALGORITHM = AES_256
+	ENCRYPTION BY CERTIFICATE EmpPass;
+GO
+
+OPEN SYMMETRIC KEY EmpPass_SM
+	DECRYPTION BY CERTIFICATE EmpPass;
+
+-- drop SYMMETRIC KEY CusInfo_SM
+
+UPDATE dbo.Employee_T 
+	set [password] = EncryptByKey(KEY_GUID('EmpPass_SM'), 
+							convert(varbinary, EmployeeID))
+GO
+
+OPEN SYMMETRIC KEY EmpPass_SM
+	DECRYPTION BY CERTIFICATE EmpPass;
+
+SELECT *, 
+    CONVERT(int, DecryptByKey([password]))   
+    AS 'Decrypted password'
+    FROM dbo.Employee_T;  
+GO  
