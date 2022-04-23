2bd864515663053d12782e2e67cddfd67c04683d
Ashwath Balaji <99102394+ashwathbalaji98@users.noreply.github.com>
2022-04-22 22:17:32 -0400

    Delete P4 - Check Constraint - Chk_DeliveryDate.sql

--- P4.-Database Implementation/SQL files/P4 - Check Constraint - Chk_DeliveryDate.sql
+++
@@ -1,8 +0,0 @@
---Check constraint - to ensure actual shipment delivery date is after the shipment order date
-alter table shipment_t
-add constraint Chk_DeliveryDate check (ActualDeliveryDate>OrderDate)
-
---Erroneous code which inserts a delivery date before the shipment order date
-update Shipment_T
-set ActualDeliveryDate='2022-02-14' 
-where ShipmentID=10001
