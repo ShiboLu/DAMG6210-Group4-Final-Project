288ea0ec97f1b493cdf1f48ca19be06b88185ce3
Ashwath Balaji <99102394+ashwathbalaji98@users.noreply.github.com>
2022-04-22 22:20:11 -0400

    Delete SQLVIEWStoreDepartment.sql

--- P4.-Database Implementation/SQL files/SQLVIEWStoreDepartment.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIEW StoreDepartmentDetails AS
-SELECT CU.StoreID, CU.StoreTelephone, CH.DepartmantName, CH.DepartmentID
-FROM Store_T CU 
-JOIN Department_T CH On CU.StoreID = CH.StoreID;
-
-SELECT * FROM StoreDepartmentDetails
-
-
-
